03e267124a6346644cef6e4fa2ec13d9eaff73e3
Trần Cao Minh Thắng <caominhthangtran@gmail.com>
2023-12-02 15:57:43 +0700

    System EduQuiz 5P

---
+++ EduQuiz 5P/EduQuiz 5P/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduQuiz_5P.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
